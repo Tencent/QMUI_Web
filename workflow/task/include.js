c514b843bd14e02a9ab5b1c42b4589484ccc72c8
kayo5994 <330956999@qq.com>
2016-08-31 17:47:55 +0800

    修复模板引擎在检测不想输出的文件时不会触发编译的问题

--- workflow/task/include.js
+++ workflow/task/include.js
@@ -1,17 +1,20 @@
 // 模板 include 命令，解释被 include 的内容并输出独立的 HTML 文件
 
+var path = require('path');
+
 module.exports = function(gulp, common) {
-  var _htmlSource;
-  if (typeof common.config.htmlSourcePath === 'object') {
-    _htmlSource = common.config.htmlSourcePath;
-  } else {
-    _htmlSource = [];
-    _htmlSource.push(common.config.htmlSourcePath);
-  }
-  _htmlSource.push('!../../**/_*.html');
-  console.log(_htmlSource);
+
   gulp.task('include', '执行模板 include 编译（建议调用 watch 任务自动监控文件变化并调用）', function() {
-    gulp.src(_htmlSource)
+
+    var _condition = function (_file) {
+      var _fileName = path.basename(_file.path);
+      if(_fileName.match(/^_/)) {
+        return false;
+      }
+      return true;
+    }
+
+    gulp.src(common.config.htmlSourcePath)
         .pipe(common.plugins.plumber({
           errorHandler: function(_error) {
             common.plugins.util.log(common.plugins.util.colors.red('QMUI Include: ') + _error);
@@ -20,7 +23,7 @@ module.exports = function(gulp, common) {
         .pipe(common.plugins.include({
           prefix: common.config.includePrefix // 模板函数的前缀
         }))
-        .pipe(gulp.dest(common.config.htmlResultPath));
+        .pipe(common.plugins.if(_condition, gulp.dest(common.config.htmlResultPath)));
 
       common.reload();
       common.plugins.util.log(common.plugins.util.colors.green('QMUI Include: ') + '根据 include 标签合并后输出新文件到 ' + common.config.htmlResultPath);
