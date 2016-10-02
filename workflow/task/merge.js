6bcbac7b7c95d5a3d7bbd6d23b1b8e96ea468974
kayo5994 <330956999@qq.com>
2016-10-02 22:33:14 +0800

    QMUI 源码合并变更时输出变更详情

--- workflow/task/merge.js
+++ workflow/task/merge.js
@@ -16,6 +16,12 @@ module.exports = function(gulp, common) {
       }
     }
 
+    var _replaceProjectParentDirectory = function(_source) {
+      // 转换为以项目根目录为开头的路径形式
+      var _projectParentDirectory = path.resolve('../../..');
+      return _source.replace(_projectParentDirectory, '').replace(/^[\\\/]/, '');
+    }
+
     // 合并文件
     for(var _key in _mergeRule) {
       // 后面变更文件时，需要的是每个文件在 HTML 中书写的路径，即相对模板文件的路径
@@ -25,12 +31,20 @@ module.exports = function(gulp, common) {
           _resultFileName = path.basename(_resultFile),
           _resultFilePath = path.dirname(_resultFile),
           _value = _mergeRule[_key], // 来源文件原始路径获取
-          _childFiles = [];
+          _childFiles = [],
+          _childFilesString; // 用于在 Log 中显示
 
       // 遍历来源文件并给每个文件加上 '../'
       for(var _i = 0; _i < _value.length; _i++) {
         var _childFilesRelative = '../' + _value[_i];
         _childFiles.push(_childFilesRelative);
+
+        // 拼接源文件名用于 Log 中显示
+        if (_i === 0) {
+          _childFilesString = _replaceProjectParentDirectory(path.resolve(_childFilesRelative));
+        } else {
+          _childFilesString = _childFilesString + ', ' + _replaceProjectParentDirectory(path.resolve(_childFilesRelative));
+        }
       }
 
       var _condition = function (_file) {
@@ -49,10 +63,13 @@ module.exports = function(gulp, common) {
           .pipe(common.plugins.concat(_resultFileName))
           .pipe(common.plugins.if(_condition, common.plugins.uglify(), common.plugins.cleanCss({compatibility: 'ie8'})))
           .pipe(gulp.dest(_resultFilePath));
+
+      common.plugins.util.log(common.plugins.util.colors.green('QMUI Merge: ') + '文件 ' + _childFilesString + ' 合并压缩为 ' + _replaceProjectParentDirectory(path.resolve(path.join(_resultFilePath, _resultFileName))));
     }
     // 变更文件引用路径
     gulp.src(common.config.htmlResultPath + '/**/*.html')
         .pipe(common.plugins.merge(_mergeRule))
         .pipe(gulp.dest(common.config.htmlResultPath));
+    common.plugins.util.log(common.plugins.util.colors.green('QMUI Merge: ') + '文件合并变更已完成');
   });
 };
