2c4ec99ca4c2f33767e1e9cc254ad2e48b2d2dcc
kayo5994 <330956999@qq.com>
2017-02-21 16:27:14 +0800

    Sass 编译加入 Debug 控制

--- workflow/task/sass.js
+++ workflow/task/sass.js
@@ -3,21 +3,22 @@ var argv = require('yargs').argv,
     lazysprite = require('postcss-lazysprite');
 
 module.exports = function(gulp, common) {
+  var _spriteConfig = {
+    cssSeparator: '_',
+    imagePath: common.config.imagesSourcePath,
+    stylesheetPath: '../../public/style/css',
+    spritePath: common.config.imagesResultPath,
+    smartUpdate: true,
+    nameSpace: common.config.prefix + '_'
+  };
   if (argv.debug) {
-  } else {
+    _spriteConfig.logLevel = 'silent';
   }
 
   gulp.task('sass', '进行 Sass 编译以及雪碧图处理（框架自带 Watch 机制监听 Sass 和图片变化后自行编译，不建议手工调用本方法）', function () {
     return gulp.src('../project/**/*.scss')
                .pipe(common.plugins.sass({outputStyle: 'expanded'}).on('error', common.plugins.sass.logError))
-               .pipe(common.plugins.postcss([lazysprite({
-                 cssSeparator: "_",
-                 imagePath: common.config.imagesSourcePath,
-                 stylesheetPath: '../../public/style/css',
-                 spritePath: common.config.imagesResultPath,
-                 smartUpdate: true,
-                 nameSpace: common.config.prefix + "_"
-               })]))
+               .pipe(common.plugins.postcss([lazysprite(_spriteConfig)]))
                .pipe(gulp.dest('../../public/style/css'));
   });
 };
