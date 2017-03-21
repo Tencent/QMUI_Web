ea4e6575520fc21db3153a782f048c3babd4b5f6
kayo5994 <330956999@qq.com>
2017-03-21 12:46:56 +0800

    合并规则示例增加 CSS 示例

--- mergeRule.js
+++ mergeRule.js
@@ -1,4 +1,5 @@
 module.exports = {
   "comment" : "以下是示例内容，使用时需要修改为项目实际需要的内容并必须删除本段注释",
   "../public/js/all.js" : ["../public/js/prettify.js", "../public/js/lang-css.js", "../public/js/main.js"]
+  "../public/style/css/main.css" : ["../public/style/css/logic.css", "../public/style/css/widget.css"]
 };
