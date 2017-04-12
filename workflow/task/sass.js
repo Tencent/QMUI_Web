8df9a228960ca7ccb9a98166aa95b3c2b0184b69
kayo5994 <330956999@qq.com>
2017-04-12 19:44:02 +0800

    1. Sass 编译优化为增量编译；2. postcss-lazysprite 升级至 1.2.3，默认不输出警告信息；

--- workflow/task/sass.js
+++ workflow/task/sass.js
@@ -19,11 +19,16 @@ module.exports = function(gulp, common) {
   }
 
   gulp.task('sass', '进行 Sass 编译以及雪碧图处理（框架自带 Watch 机制监听 Sass 和图片变化后自行编译，不建议手工调用本方法）', function () {
+    var _isOpeningBrowserSyncMod = common.config.browserSyncMod !== 'close';
     return gulp.src('../project/**/*.scss')
                .pipe(common.plugins.if(common.config.needsSourceMaps, common.plugins.sourcemaps.init()))
+               .pipe(common.plugins.if(global.isWatching, common.plugins.cached('sass')))
+               .pipe(common.plugins.sassInheritance({base: '../project/'}))
+               .pipe(common.plugins.if(!!argv.debug, common.plugins.debug({title: 'Sass Debug:'})))
                .pipe(common.plugins.sass({outputStyle: 'expanded'}).on('error', common.plugins.sass.logError))
                .pipe(common.plugins.postcss([lazysprite(_spriteConfig)]))
                .pipe(common.plugins.if(common.config.needsSourceMaps, common.plugins.sourcemaps.write('./maps'))) // Source Maps 的 Base 输出目录为 style 输出的目录
-               .pipe(gulp.dest(_styleResultPath));
+               .pipe(gulp.dest(_styleResultPath))
+               .pipe(common.plugins.if(_isOpeningBrowserSyncMod, common.reload({stream: true})));
   });
 };
