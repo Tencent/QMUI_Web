ec4303d17138388e3fd4cf7c725d1b8d62e25396
kayo5994 <330956999@qq.com>
2017-04-15 17:14:55 +0800

    同步图片和图片压缩功能加开关

--- workflow/task/watch.js
+++ workflow/task/watch.js
@@ -41,74 +41,76 @@ module.exports = function(gulp, common) {
         _independentImagesResultPath = common.config.imagesResultPath + common.config.independentImagesDirectory,
         _shouldOutputEmptyLineForSyncImage;
 
-    // 如果有需要，则在执行同步图片任务之前输出一个空行
-    var _outputEmptyForSyncImageIfNeed = function() {
-      if(_shouldOutputEmptyLineForSyncImage) {
-        common.log('');
-        _shouldOutputEmptyLineForSyncImage = false;
-      }
-    }
-
-    // 压缩独立图片并同步独立图片到 public 目录
-    gulp.watch([_independentImagesSourcePath, _independentImagesSourcePath + '/**/*'], function() {
-      _shouldOutputEmptyLineForSyncImage = true;
-      common.plugins.fileSync(_independentImagesSourcePath, _independentImagesResultPath, {
-        ignore: ['.DS_Store', '.svn', '.git'],
-        beforeAddFileCallback: function(_fullPathSrc) {
-         var _absoluteMinImageFilePath = path.resolve(_fullPathSrc),
-             _absoluteMinImageFilePathMd5 = md5(_absoluteMinImageFilePath);
-
-         if (!common.lib.isElementInArray(justBeforeAddedImage, _absoluteMinImageFilePathMd5)) {
-           // 这里为了避免发生“增加图片到 public/images 或修改 public/images 原有的图片，触发压缩图片，因此图片又被修改，再次触发压缩图片”的情况发生，
-           // 做了一个判断，压缩一张图片时会标记下来，当再次发生图片改变时会判断这张图片是否为刚刚压缩过的图片，如果是则不执行该次压缩图片的逻辑
-           // 如果不是，则说明准备处理另一张图片了，这时清空标记，进入下一张图片的处理
-           justBeforeAddedImage.push(_absoluteMinImageFilePathMd5);
-           _outputEmptyForSyncImageIfNeed();
-           common.log('Min Image', '对 ' + _absoluteMinImageFilePath + ' 进行图片压缩');
-           _imageMinOnSameDir(_absoluteMinImageFilePath);
-         }
-        },
-        beforeUpdateFileCallback: function(_fullPathSrc) {
-         var _absoluteMinImageFilePath = path.resolve(_fullPathSrc),
-             _absoluteMinImageFilePathMd5 = md5(_absoluteMinImageFilePath);
-
-         if (!common.lib.isElementInArray(justBeforeAddedImage, _absoluteMinImageFilePathMd5)) {
-           justBeforeAddedImage.push(_absoluteMinImageFilePathMd5);
-           _outputEmptyForSyncImageIfNeed();
-           common.log('Min Image', '对 ' + _absoluteMinImageFilePath + ' 进行图片压缩');
-           _imageMinOnSameDir(_absoluteMinImageFilePath);
-         } else {
-           justBeforeAddedImage = common.lib.deleteElementInArray(justBeforeAddedImage, _absoluteMinImageFilePathMd5);
-         }
-        },
-        addFileCallback: function(_fullPathSrc, _fullPathDist) {
-         var _absoluteMinImageFilePath = path.resolve(_fullPathDist),
-             _absoluteMinImageFilePathMd5 = md5(_absoluteMinImageFilePath);
-
-         if (!common.lib.isElementInArray(justAddedImage, _absoluteMinImageFilePathMd5)) {
-           justAddedImage.push(_absoluteMinImageFilePathMd5);
-           _outputEmptyForSyncImageIfNeed();
-           common.log('Sync Image', '同步增加文件到 ' + _absoluteMinImageFilePath);
-         }
-        },
-        deleteFileCallback: function(_fullPathSrc, _fullPathDist) {
-         _outputEmptyForSyncImageIfNeed();
-         common.log('Sync Image', '同步删除文件 ' + path.resolve(_fullPathDist));
-        },
-        updateFileCallback: function(_fullPathSrc, _fullPathDist) {
-         var _absoluteMinImageFilePath = path.resolve(_fullPathDist),
-             _absoluteMinImageFilePathMd5 = md5(_absoluteMinImageFilePath);
-
-         if (!common.lib.isElementInArray(justAddedImage, _absoluteMinImageFilePathMd5)) {
-           justAddedImage.push(_absoluteMinImageFilePathMd5);
-           _outputEmptyForSyncImageIfNeed();
-           common.log('Sync Image', '同步更新文件到 ' + _absoluteMinImageFilePath);
-         } else {
-           justAddedImage = common.lib.deleteElementInArray(justAddedImage, _absoluteMinImageFilePathMd5);
-         }
+    if (common.config.needsImagesMinAndSync) {
+      // 如果有需要，则在执行同步图片任务之前输出一个空行
+      var _outputEmptyForSyncImageIfNeed = function() {
+        if(_shouldOutputEmptyLineForSyncImage) {
+          common.log('');
+          _shouldOutputEmptyLineForSyncImage = false;
         }
+      }
+
+      // 压缩独立图片并同步独立图片到 public 目录
+      gulp.watch([_independentImagesSourcePath, _independentImagesSourcePath + '/**/*'], function() {
+        _shouldOutputEmptyLineForSyncImage = true;
+        common.plugins.fileSync(_independentImagesSourcePath, _independentImagesResultPath, {
+          ignore: ['.DS_Store', '.svn', '.git'],
+          beforeAddFileCallback: function(_fullPathSrc) {
+           var _absoluteMinImageFilePath = path.resolve(_fullPathSrc),
+               _absoluteMinImageFilePathMd5 = md5(_absoluteMinImageFilePath);
+
+           if (!common.lib.isElementInArray(justBeforeAddedImage, _absoluteMinImageFilePathMd5)) {
+             // 这里为了避免发生“增加图片到 public/images 或修改 public/images 原有的图片，触发压缩图片，因此图片又被修改，再次触发压缩图片”的情况发生，
+             // 做了一个判断，压缩一张图片时会标记下来，当再次发生图片改变时会判断这张图片是否为刚刚压缩过的图片，如果是则不执行该次压缩图片的逻辑
+             // 如果不是，则说明准备处理另一张图片了，这时清空标记，进入下一张图片的处理
+             justBeforeAddedImage.push(_absoluteMinImageFilePathMd5);
+             _outputEmptyForSyncImageIfNeed();
+             common.log('Min Image', '对 ' + _absoluteMinImageFilePath + ' 进行图片压缩');
+             _imageMinOnSameDir(_absoluteMinImageFilePath);
+           }
+          },
+          beforeUpdateFileCallback: function(_fullPathSrc) {
+           var _absoluteMinImageFilePath = path.resolve(_fullPathSrc),
+               _absoluteMinImageFilePathMd5 = md5(_absoluteMinImageFilePath);
+
+           if (!common.lib.isElementInArray(justBeforeAddedImage, _absoluteMinImageFilePathMd5)) {
+             justBeforeAddedImage.push(_absoluteMinImageFilePathMd5);
+             _outputEmptyForSyncImageIfNeed();
+             common.log('Min Image', '对 ' + _absoluteMinImageFilePath + ' 进行图片压缩');
+             _imageMinOnSameDir(_absoluteMinImageFilePath);
+           } else {
+             justBeforeAddedImage = common.lib.deleteElementInArray(justBeforeAddedImage, _absoluteMinImageFilePathMd5);
+           }
+          },
+          addFileCallback: function(_fullPathSrc, _fullPathDist) {
+           var _absoluteMinImageFilePath = path.resolve(_fullPathDist),
+               _absoluteMinImageFilePathMd5 = md5(_absoluteMinImageFilePath);
+
+           if (!common.lib.isElementInArray(justAddedImage, _absoluteMinImageFilePathMd5)) {
+             justAddedImage.push(_absoluteMinImageFilePathMd5);
+             _outputEmptyForSyncImageIfNeed();
+             common.log('Sync Image', '同步增加文件到 ' + _absoluteMinImageFilePath);
+           }
+          },
+          deleteFileCallback: function(_fullPathSrc, _fullPathDist) {
+           _outputEmptyForSyncImageIfNeed();
+           common.log('Sync Image', '同步删除文件 ' + path.resolve(_fullPathDist));
+          },
+          updateFileCallback: function(_fullPathSrc, _fullPathDist) {
+           var _absoluteMinImageFilePath = path.resolve(_fullPathDist),
+               _absoluteMinImageFilePathMd5 = md5(_absoluteMinImageFilePath);
+
+           if (!common.lib.isElementInArray(justAddedImage, _absoluteMinImageFilePathMd5)) {
+             justAddedImage.push(_absoluteMinImageFilePathMd5);
+             _outputEmptyForSyncImageIfNeed();
+             common.log('Sync Image', '同步更新文件到 ' + _absoluteMinImageFilePath);
+           } else {
+             justAddedImage = common.lib.deleteElementInArray(justAddedImage, _absoluteMinImageFilePathMd5);
+           }
+          }
+        });
       });
-    });
+    }
 
     // 雪碧图与样式处理
     // 监控雪碧图原图和样式，如果有改动，会触发样式编译以及雪碧图生成
@@ -120,24 +122,26 @@ module.exports = function(gulp, common) {
     });
 
     // 压缩雪碧图
-    var _minImageWatcher = gulp.watch(common.config.imagesResultPath + '/*.*');
-    _minImageWatcher.on('change', function(event) {
-      var _minImageFile = event.path,
-          _minImageFilePathMd5 = md5(_minImageFile);
-      // 这里为了避免发生“增加图片到 public/images 或修改 public/images 原有的图片，触发压缩图片，因此图片又被修改，再次触发压缩图片”的情况发生，
-      // 做了一个判断，压缩一张图片时会标记下来，当再次发生图片改变时会判断这张图片是否为刚刚压缩过的图片，如果是则不执行该次压缩图片的逻辑
-      // 如果不是，则说明准备处理另一张图片了，这时清空标记，进入下一张图片的处理
-      if (event.type !== 'deleted' && !common.lib.isElementInArray(justAddedImage, _minImageFilePathMd5)) {
+    if (common.config.needsImagesMinAndSync) {
+      var _minImageWatcher = gulp.watch(common.config.imagesResultPath + '/*.*');
+      _minImageWatcher.on('change', function(event) {
+        var _minImageFile = event.path,
+            _minImageFilePathMd5 = md5(_minImageFile);
+        // 这里为了避免发生“增加图片到 public/images 或修改 public/images 原有的图片，触发压缩图片，因此图片又被修改，再次触发压缩图片”的情况发生，
+        // 做了一个判断，压缩一张图片时会标记下来，当再次发生图片改变时会判断这张图片是否为刚刚压缩过的图片，如果是则不执行该次压缩图片的逻辑
+        // 如果不是，则说明准备处理另一张图片了，这时清空标记，进入下一张图片的处理
+        if (event.type !== 'deleted' && !common.lib.isElementInArray(justAddedImage, _minImageFilePathMd5)) {
 
-        justAddedImage.push(_minImageFilePathMd5);
+          justAddedImage.push(_minImageFilePathMd5);
 
-        common.log('Min Image', '对 ' + _minImageFile + ' 进行图片压缩');
-        _imageMinOnSameDir(_minImageFile);
+          common.log('Min Image', '对 ' + _minImageFile + ' 进行图片压缩');
+          _imageMinOnSameDir(_minImageFile);
 
-      } else if (common.lib.isElementInArray(justAddedImage, _minImageFilePathMd5)) {
-        justAddedImage = common.lib.deleteElementInArray(justAddedImage, _minImageFilePathMd5);
-      }
-    });
+        } else if (common.lib.isElementInArray(justAddedImage, _minImageFilePathMd5)) {
+          justAddedImage = common.lib.deleteElementInArray(justAddedImage, _minImageFilePathMd5);
+        }
+      });
+    }
 
     // 模板自动 include
     if (common.config.openIncludeFunction) {
