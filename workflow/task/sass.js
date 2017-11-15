3d7fa44bad11fb56867b1b8251b71d854a98de10
kayo5994 <330956999@qq.com>
2017-11-15 12:59:24 +0800

    update: update autoprefixer's browerslist and add some error protection for sass compilation

--- workflow/task/sass.js
+++ workflow/task/sass.js
@@ -15,8 +15,8 @@
 
 // 进行 Sass 编译以及雪碧图处理
 var argv = require('yargs').argv,
-  lazysprite = require('postcss-lazysprite');
-autoprefixer = require('autoprefixer');
+  lazysprite = require('postcss-lazysprite'),
+  autoprefixer = require('autoprefixer');
 
 module.exports = function (gulp, common) {
   var _spriteConfig = {
@@ -38,14 +38,23 @@ module.exports = function (gulp, common) {
   gulp.task('sass', '进行 Sass 编译以及雪碧图处理（框架自带 Watch 机制监听 Sass 和图片变化后自行编译，不建议手工调用本方法）', function () {
     var _isOpeningBrowserSyncMod = common.config.browserSyncMod !== 'close';
     return gulp.src('../project/**/*.scss')
+      .pipe(common.plugins.plumber({
+        errorHandler: function(_error) {
+          common.error('SASS', _error);
+          common.plugins.util.beep();
+        }}))
       .pipe(common.plugins.if(common.config.needsSourceMaps, common.plugins.sourcemaps.init()))
       .pipe(common.plugins.if(global.isWatching && global.isHandleStyle, common.plugins.cached('sass')))
       .pipe(common.plugins.sassInheritance({base: '../project/'}))
       .pipe(common.plugins.if(Boolean(argv.debug), common.plugins.debug({title: 'Sass Debug:'})))
       .pipe(common.plugins.sass({outputStyle: 'expanded'}).on('error', common.plugins.sass.logError))
+      .on('error', function(err) {
+        common.error('Sass Error', err.message);
+        this.end();
+      })
       .pipe(common.plugins.postcss([lazysprite(_spriteConfig), autoprefixer({
         flexbox: false,
-        browsers: ['last 5 versions', 'IE 8', 'IE 9', 'IE 10']
+        browsers: ['defaults', 'last 5 versions', '> 5% in CN', 'not ie < 8']
       })]))
       .pipe(common.plugins.if(common.config.needsSourceMaps, common.plugins.sourcemaps.write('./maps'))) // Source Maps 的 Base 输出目录为 style 输出的目录
       .pipe(gulp.dest(_styleResultPath))
